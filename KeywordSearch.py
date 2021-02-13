0034246a2c322c202c0e7bc7ae3627113431ba17
heyzues <47094613+KillzMckinzie@users.noreply.github.com>
2021-02-12 22:26:52 -0600

    Searches directories for sensitive words
    It's not done, I need to add a way to search through specific drives and folders. That might be done elsewhere though, like when you initially open the program like "Full scan, partial scan (this will auto search through directories we think to look through), or specific (and allow user to choose which one)

---
+++ KeywordSearch.py
@@ -0,0 +1,64 @@
+import os
+import win32api
+import re # need to test keywords in each string
+
+
+
+def findDrives():
+    #make sure to import win32api at the top
+    drives = win32api.GetLogicalDriveStrings()
+    drives = drives.split('\000')[:-1]
+    print(drives)
+
+    
+    
+def iterateThroughFilesFolders(): # This will iterate through a chosen directory to get filenames
+    
+    fileListWithDirectory = []
+    
+    for root, path, file in os.walk("D://Test//"):
+        for filename in file:
+            if root[-2:-1] == '/':
+                fileListWithDirectory.append(root+filename)
+            else:
+                fileListWithDirectory.append(root+'//'+filename)
+                
+    return fileListWithDirectory
+                
+
+def keyWordSearch(x):
+
+    wordList = ['important','password','private','bank',
+                'hidden','phone','credit','card','paypal',
+                'email','backup','nude','hidden','porn',
+                'finance','purchase','delete','mastercard',
+                'visa','passport','identification',
+                'username','login','ssn','security',
+                'confidential','discover','secret','personal',
+                'secure','registration','doctor','taxes',
+                'financial','pin','receipt','vin','final']
+   
+    
+    for file in x:
+        base = os.path.basename(file) #removing the directory
+        temp1 = os.path.splitext(base) # this takes the file name 'example.jpg' and splits it into a list ['example','.jpg']
+
+        
+            
+
+        cleanFilename = re.sub('\W+',' ',temp1[0]) #using regex to remove special characters
+        
+
+        for word in wordList:
+            if word in cleanFilename.lower():
+                print('Possible Sensitive Files Found: '+ file)
+                break 
+            else:
+                continue # Continues
+
+def main():
+    #findDrives()
+    keyWordSearch(iterateThroughFilesFolders())
+
+
+main()
