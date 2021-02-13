c4bc0ce003481b57e12828943a1525b2c1351d9c
heyzues <47094613+KillzMckinzie@users.noreply.github.com>
2021-02-12 22:31:15 -0600

    Update KeywordSearch.py

--- KeywordSearch.py
+++ KeywordSearch.py
@@ -2,20 +2,15 @@ import os
 import win32api # Used for the 'findDrives()' function. Not used though, but could be implemented here or somewhere else later
 import re # needed to test keywords in each string
 
-
-
 def findDrives():
     #make sure to import win32api at the top
     drives = win32api.GetLogicalDriveStrings()
     drives = drives.split('\000')[:-1]
     print(drives)
-
-    
     
 def iterateThroughFilesFolders(): # This will iterate through a chosen directory to get filenames
-    
     fileListWithDirectory = []
-    
+ 
     for root, path, file in os.walk("D://Test//"):
         for filename in file:
             if root[-2:-1] == '/':
@@ -27,7 +22,6 @@ def iterateThroughFilesFolders(): # This will iterate through a chosen directory
                 
 
 def keyWordSearch(x):
-
     wordList = ['important','password','private','bank',
                 'hidden','phone','credit','card','paypal',
                 'email','backup','nude','hidden','porn',
@@ -38,16 +32,11 @@ def keyWordSearch(x):
                 'secure','registration','doctor','taxes',
                 'financial','pin','receipt','vin','final']
    
-    
     for file in x:
         base = os.path.basename(file) #removing the directory
         temp1 = os.path.splitext(base) # this takes the file name 'example.jpg' and splits it into a list ['example','.jpg']
 
-        
-            
-
         cleanFilename = re.sub('\W+',' ',temp1[0]) #using regex to remove special characters
-        
 
         for word in wordList:
             if word in cleanFilename.lower():
