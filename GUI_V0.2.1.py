805282b05e49d491e6071ce758a040bae647e244
heyzues <jborne404@gmail.com>
2021-03-10 20:05:38 -0600

    added right click menu

--- GUI_V0.2.1.py
+++ GUI_V0.2.1.py
@@ -6,6 +6,7 @@ class Application(tk.Frame):
     #directory = tk.StringVar()
     foundFiles = []
     folder_selected = ''
+    full_scan = False # for the popup message, but I commeneted out so no use atm
     
 
     def __init__(self, root):
@@ -24,7 +25,8 @@ class Application(tk.Frame):
         self.root.geometry("{}x{}+{}+{}".format(self.windowWidth, self.windowHeight, self.xCordinate, self.yCordinate))
 
         # Create a style
-        self.style = ttk.Style(self.root)
+        #self.style = ttk.Style(self.root)
+        #self.root.configure(bg="gray26")
 
         # Import the tcl file then Set the theme with the theme_use method
         # self.root.tk.call('source', 'azure-dark.tcl')
@@ -47,8 +49,9 @@ class Application(tk.Frame):
         self.h = tk.IntVar()
 
         # Frame for the Radiobuttons and entry 
-        self.radioframe = ttk.LabelFrame(self.root, text='Scan Type', width=410, height=200)
+        self.radioframe = tk.LabelFrame(self.root, text='Scan Type', width=410, height=200)
         self.radioframe.place(x=10, y=10)
+        #self.radioframe.configure(bg="gray26",)
 
         # Radiobuttons
         self.radio0 = ttk.Radiobutton(self.radioframe, text='Quick Scan', variable=self.d, value=1)
@@ -80,15 +83,15 @@ class Application(tk.Frame):
         self.exitButton.place(x=335,y=220)
 
         # TEMP BUTTON
-        self.tempButton = ttk.Button(self.root, text='TEMP', command=self.insert_data)
+        self.tempButton = ttk.Button(self.root, text='Options', command=self.optionsButtonActions())
         self.tempButton.place(x=190,y=220)
 
         # Notebook
-        self.notebook = ttk.Notebook(self.root, width=1000, height=780)
+        self.notebook = ttk.Notebook(self.root)
         self.notebook.place(x=440,y=19)
 
         #Notebook tabs
-        self.notebookTab1 = ttk.Frame(self.notebook,width=1000, height=780)
+        self.notebookTab1 = ttk.Frame(self.notebook)
         self.notebook.add(self.notebookTab1, text='  Files  ')
 
         self.notebookTab2 = ttk.Frame(self.notebook)
@@ -98,7 +101,7 @@ class Application(tk.Frame):
         self.notebook.add(self.notebookTab3, text='  Other  ')
 
         # Treeview
-        self.treeview = ttk.Treeview(self.notebookTab2,height=30)
+        self.treeview = ttk.Treeview(self.notebookTab2,height=38)
         self.treeview.pack()
 
         self.treeview.column("#0",width=1000)
@@ -129,6 +132,64 @@ class Application(tk.Frame):
         self.lbl_3 = ttk.Label(self.infoframe, text=" • Partial scan allows you to define the directory.")
         self.lbl_3.place(x=7, y=60)
 
+        # Bind our functions to the Treeview.
+        self.treeview.bind("<Button-3>", self.preClick)
+        self.treeview.bind("<Button-1>", self.onLeft)
+
+    # Right Click menu
+    def onRight(self, *args):
+        # Here we fetch our X and Y coordinates of the cursor RELATIVE to the window
+        cursorx = int(self.root.winfo_pointerx() - self.root.winfo_rootx())
+        cursory = int(self.root.winfo_pointery() - self.root.winfo_rooty())
+
+        # Now we define our right click menu canvas
+        self.onRight_menu = tk.Canvas(self.root, width=150, height=50, highlightbackground="gray", highlightthickness=1)
+        # And here is where we use our X and Y variables, to place the menu where our cursor is,
+        # That's how right click menus should be placed.
+        self.onRight_menu.place(x=cursorx, y=cursory)
+        # This is for packing our options onto the canvas, to prevent the canvas from resizing.
+        # This is extremely useful if you split your program into multiple canvases or frames
+        # and the pack method is forcing them to resize.
+        self.onRight_menu.pack_propagate(0)
+        # Here is our label on the right click menu for deleting a row, notice the cursor
+        # value, which will give us a pointy finger when you hover over the option.
+        delLabel = tk.Label(self.onRight_menu, text="Ignore Directory", cursor="hand2", anchor="w")
+        delLabel.pack(side="top", padx=1, pady=1, fill="x")
+
+        # This function is for removing the canvas when an option is clicked.
+        def destroy(self):
+            self.onRight_menu.place_forget()
+
+        # This is the function that removes the selected item when the label is clicked.
+        def delete(*args):
+            selection = self.treeview.selection()
+            #self.treeview.delete(selection)
+            scan = scanner.Scanner(self.entry.get())
+            scan.ignoreThisDirectory(Path.parent(selection))
+            try:
+                self.onRight_menu.destroy()
+            except Exception:
+                pass
+            print(selection,"Has been deleted")
+
+        delLabel.bind("<Button-1>", delete)
+
+    # This is to prevent infinite right click menus; it sees if there is an existing menu
+    # and removes it, bringing it out in a new position.
+    def preClick(self,*args):
+        try:
+            self.onRight_menu.place_forget()
+            self.onRight()
+        except Exception:
+            self.onRight()
+
+    # Hide menu when left clicking
+    def onLeft(self, *args):
+        try:
+            self.onRight_menu.place_forget()
+        except Exception:
+            pass
+
     # Check Radiobutton selection
     def checkRadiobutton(self):
         if self.d.get() == 1:
@@ -140,6 +201,18 @@ class Application(tk.Frame):
     
     # Scan button function call
     def scanButtonActions(self):
+        if self.checkRadiobutton() == 1:
+            # self.popupmsg("    This could take a very long time, continue?")
+
+            # if full_scan:
+            #     full_scan = False
+            #     print("Full Scan ititiated")
+
+            pass
+        
+        if self.checkRadiobutton() == 2:
+            pass
+                
         if self.checkRadiobutton() == 3:
             self.text.delete('1.0',END) # Remove what's currently in entry widget
             scan = scanner.Scanner(self.entry.get()) # creating scan object
@@ -175,6 +248,34 @@ class Application(tk.Frame):
     # Inserting data into treeview
     def insert_data(self,file, parentNumber):         
         self.treeview.insert(parent=parentNumber, index='end', text=str(file))
+    
+    # Pop up message
+    # def popupmsg(self,msg):
+    #     popup = tk.Tk()
+    #     popup.title("Info")
+    #     popupWidth = 250
+    #     popupHeight = 100
+    #     screenWidth = self.root.winfo_screenwidth()
+    #     screenHeight = self.root.winfo_screenheight()
+    #     xCordinate = int((screenWidth/2) - (popupWidth/2))
+    #     yCordinate = int((screenHeight/2) - (popupHeight/2))
+    #     popup.geometry("{}x{}+{}+{}".format(popupWidth, popupHeight, xCordinate, yCordinate))
+    #     label = ttk.Label(popup, text=msg)
+    #     label.pack(side="top", fill="x", pady=10)
+    #     B1 = ttk.Button(popup, text="Scan", command=lambda: [self.popupmsgButtonActions(), popup.destroy()])
+    #     B1.place(x=10,y=50)
+    #     B2 = ttk.Button(popup, text="Quit", command=lambda: [popup.destroy()])
+    #     B2.place(x=120,y=50)
+    #     popup.mainloop()
+    
+    # def popupmsgButtonActions(self):
+    #     full_scan = True
+
+    def optionsButtonActions(self):
+        pass
+    
+
+        
 
 app = Application(tk.Tk())
 app.root.mainloop()        
--- scanner.py
+++ scanner.py
@@ -2,15 +2,22 @@ from pathlib import Path
 import re
 class Scanner:
 
+    ignored_directories = []
+
     def __init__(self, p):
         self.p = Path(p)
 
     def directory_file_iteration(self):
+        self.readInDirectories()
         filesFound = []
         for i in self.p.rglob("*"):
-            if i.is_file():
-                if self.keywordSearch(i):
-                    filesFound.append(i)
+            if str(i.parents[0]) in self.ignored_directories: # allow you to ignore certain directories
+                print("Ignored", i)
+                pass
+            else:
+                if i.is_file():
+                    if self.keywordSearch(i):
+                        filesFound.append(i)
         return filesFound
     
     def keywordSearch(self,i):
@@ -20,29 +27,31 @@ class Scanner:
         wordList = ['important','password','private','bank',
             'hidden','phone','credit card','paypal',
             'email','backup','nude','hidden','porn',
-            'finance','purchase','delete','mastercard',
+            'finance','purchase','mastercard',
             'visa','passport','identification',
-            'username','login','ssn','security',
+            'username','login','ssn','secur',
             'confidential','discover','secret','personal',
             'secure','registration','doctor','taxes',
             'financial','pin ','receipt','vin ','tax','resume',
-            'doctor','medic','money','key','contacts']
+            'doctor','medic','money','contact','sensitive']
 
         if i.match("*.txt"): # Finding and reading in text files for keywords
             try:
-                d = i.read_text()
+                d = i.read_text(encoding='utf-8')
                 for word in wordList:
                     if word in d.lower():
                         pass
                         #print("Found:",word," In:",i,"Contents:",d)
                 self.ssnSearch(d)
-            except UnicodeDecodeError:
-                print("Can't read file:",i.name)
+            except UnicodeDecodeError as e:
+                print("Error:",e,"Can't read file:",i)
         
         for word in wordList:
             if word in str(i.name).lower(): # checking filename
                 if i:
                     return True
+            else:
+                return True
     
     # searching for SSNs
     def ssnSearch(self,d):
@@ -54,6 +63,21 @@ class Scanner:
                 pass
             else:
                 print("Possible SSN:",i)
+    
+    # Ignore_dir.txt which will hold directories you want to ignore
+    def readInDirectories(self):
+        f = open("ignored_dir.txt","r")
+        for x in f:
+            self.ignored_directories.append(x)
+        f.close()
+
+        for i in self.ignored_directories:
+            print("Ignored dir:",i)
+    
+    def ignoreThisDirectory(self,i):
+        f = open("ignore_dir.txt","w")
+        f.write(i)
+        f.close()
 
 
     def get_scanning(self):
